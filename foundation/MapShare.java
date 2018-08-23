e1e68ccd89f7e7b3cc8a74ae5f232309fbcde42c
Samanta <Arajit.Samanta@ehi.com>
2018-08-23 14:06:51 -0500

    map  concurrent update problem

---
+++ foundation/MapShare.java
@@ -0,0 +1,44 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+class MapShare {
+
+    public static void main(String[] args) {
+        Map<String, String> input = new HashMap<>();
+        //input.put("a", "aaa");
+        input.put("b", "xyz");
+        input.put("c", "ccc");
+
+        MapShare obj = new MapShare();
+
+        Map<String, String> op = obj.mapShare(input);
+        op.forEach((k, v) -> {
+            System.out.println(k + " " + v);
+        });
+    }
+
+    public Map<String, String> mapShare(Map<String, String> map) {
+        Map<String, String> resultMap = new HashMap<>();
+        for (String key : map.keySet()) {
+            switch (key) {
+            case "a":
+                String val = map.get(key);
+                resultMap.put(key, val);
+                if (null != val) {
+                    resultMap.put("b", val);
+                }
+                break;
+            case "b":
+                if(!resultMap.containsKey(key))
+                    resultMap.put(key, map.get(key));
+                break;
+            case "c":
+                break;
+            default:
+                resultMap.put(key, map.get(key));
+            }
+        }
+        return resultMap;
+    }
+}
