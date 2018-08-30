e6afbc38c12ba6e5ebfe0f3d38053749d435a3f0
Samanta <Arajit.Samanta@ehi.com>
2018-08-30 09:55:59 -0500

    Black jack problem

---
+++ foundation/BlackJack.java
@@ -0,0 +1,30 @@
+
+class BlackJack {
+
+  public static void main(String[] args) {
+    System.out.println(blackjack(19, 22));
+  }
+
+  public static int blackjack(int a, int b) {
+    int target = 21;
+    int result;
+    int diffA = target - a;
+    int diffB = target - b;
+
+    if (diffA < 0 && diffB < 0) {
+      result = 0;
+    } else if (diffA < 0) {
+      result = b;
+    } else if (diffB < 0) {
+      result = a;
+    } else {
+      if (diffA >= 0 && diffA < diffB) {
+        result = a;
+      } else {
+        result = b;
+      }
+    }
+    return result;
+  }
+
+}
